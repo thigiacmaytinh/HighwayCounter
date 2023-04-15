4b460e299b9809408aa18cc9b713574ed4faaa41
DESKTOP-DK7UO56\z230 <tbthanha19055@gmail.com>
2023-04-15 21:54:18 +0700

    + add data to train

--- training.bat
+++ training.bat
@@ -1,4 +1,4 @@
 DEL .\images\train.cache /F /Q
 DEL .\images\valid.cache /F /Q
-python train.py --batch 8 --cfg cfg/training/yolov7.yaml --epochs 300 --data ./images/data.yaml --weights 'yolov7.pt' --device 0 --name foambike --hyp data/hyp.scratch.p6.yaml --workers 2
+python train.py --batch 8 --cfg cfg/training/yolov7.yaml --epochs 300 --data ./images/data.yaml --weights 'yolov7.pt' --device 0 --name highway --hyp data/hyp.scratch.p6.yaml --workers 2
 pause
